d8d105446a8d5e370ae0521c115beb2a84216f34
Vitinho 21 <vbluis.play@gmail.com>
2020-11-24 10:02:45 -0300

    Adiciona arquivo SwiftUI do Onboarding

---
+++ Maozinha/onboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  onboardingView.swift
+//  Maozinha
+//
+//  Created by Vítor Bárrios Luís de Albuquerque on 24/11/20.
+//
+
+import SwiftUI
+
+struct onboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct onboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        onboardingView()
+    }
+}
