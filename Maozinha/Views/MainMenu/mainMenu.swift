579318fe03c456ed15857d9ad874b64582c5d2a4
Íris Soares <iss3@cin.ufpe.br>
2020-12-04 22:03:23 -0300

    termina cards e começa menu principal

--- Maozinha/Views/MainMenu/mainMenu.swift
+++ Maozinha/Views/MainMenu/mainMenu.swift
@@ -8,6 +8,31 @@
 import SwiftUI
 
 struct mainMenu: View {
+    
+    @State private var colorStart = ["ElementGreenBackgroundStart",
+                                     "ElementPinkBackgroundStart",
+                                     "ElementYellowBackgroundStart",
+                                     "ElementCyanBackgroundStart"]
+    @State private var colorEnd = ["ElementGreenBackgroundEnd",
+                                   "ElementPinkBackgroundEnd",
+                                   "ElementYellowBackgroundEnd",
+                                   "ElementCyanBackgroundEnd"]
+    @State private var title = ["Anúncios na\nInternet",
+                                "Notificações e\navisos",
+                                "Navegação e\nsegurança na\ninternet",
+                                "Configurações\ndo iPhone"]
+    @State private var colorTitle = ["ElementGreenText",
+                                     "ElementPinkText",
+                                     "ElementYellowText",
+                                     "ElementCyanText"]
+    @State private var threeLines = [false, false, true, false]
+    @State private var green = [true, false, false, false, false]
+    @State private var yellow = [false, false, true, false]
+    @State private var icon = ["IconWindow",
+                                "IconBell",
+                                "IconGlobe",
+                                "IconCog"]
+    
     var body: some View {
         VStack{
             HStack{
@@ -21,13 +46,23 @@ struct mainMenu: View {
             GeometryReader { geometry in
                 VStack{
                     HStack{
-                        testcyan()
+                        card(colorStart: Binding.constant(colorStart[0]), colorEnd: Binding.constant(colorEnd[0]), title: Binding.constant(title[0]), colorTitle: Binding.constant(colorTitle[0]), threeLines: Binding.constant(threeLines[0]), green: Binding.constant(green[0]), yellow: Binding.constant(yellow[0]), icon: Binding.constant(icon[0]))
                             .frame(width: (geometry.size.width / 2.2),
                                    height: (geometry.size.height / 5))
-                        test()
+                        card(colorStart: Binding.constant(colorStart[1]), colorEnd: Binding.constant(colorEnd[1]), title: Binding.constant(title[1]), colorTitle: Binding.constant(colorTitle[1]), threeLines: Binding.constant(threeLines[1]), green: Binding.constant(green[1]), yellow: Binding.constant(yellow[1]), icon: Binding.constant(icon[1]))
                             .frame(width: (geometry.size.width / 2.2),
                                    height: (geometry.size.height / 5))
                     }
+                    HStack{
+                        card(colorStart: Binding.constant(colorStart[2]), colorEnd: Binding.constant(colorEnd[2]), title: Binding.constant(title[2]), colorTitle: Binding.constant(colorTitle[2]), threeLines: Binding.constant(threeLines[2]), green: Binding.constant(green[2]), yellow: Binding.constant(yellow[2]), icon: Binding.constant(icon[2]))
+                            .frame(width: (geometry.size.width / 2.2),
+                                   height: (geometry.size.height / 5))
+                        
+                        card(colorStart: Binding.constant(colorStart[3]), colorEnd: Binding.constant(colorEnd[3]), title: Binding.constant(title[3]), colorTitle: Binding.constant(colorTitle[3]), threeLines: Binding.constant(threeLines[3]), green: Binding.constant(green[3]), yellow: Binding.constant(yellow[3]), icon: Binding.constant(icon[3]))
+                            .frame(width: (geometry.size.width / 2.2),
+                                   height: (geometry.size.height / 5))
+                    }
+                    .padding(.top, 40)
                 }
             }
         }
