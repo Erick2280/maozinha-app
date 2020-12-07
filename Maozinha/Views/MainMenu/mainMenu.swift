c7b86cc5dbe2047971c7dfb83a7322b7f2e433f5
Íris Soares <iss3@cin.ufpe.br>
2020-12-07 19:27:41 -0300

    integra menu com onboarding usando NavigationLink

--- Maozinha/Views/MainMenu/mainMenu.swift
+++ Maozinha/Views/MainMenu/mainMenu.swift
@@ -29,42 +29,59 @@ struct mainMenu: View {
     @State private var green = [true, false, false, false, false]
     @State private var yellow = [false, false, true, false]
     @State private var icon = ["IconWindow",
-                                "IconBell",
-                                "IconGlobe",
-                                "IconCog"]
+                               "IconBell",
+                               "IconGlobe",
+                               "IconCog"]
     
     var body: some View {
-        VStack{
-            HStack{
-                Text("O que vamos \naprender hoje?")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("TitleText"))
-                    .padding(.leading, 22.0)
-                Spacer()
-            }
-            GeometryReader { geometry in
-                VStack{
-                    HStack{
-                        card(colorStart: Binding.constant(colorStart[0]), colorEnd: Binding.constant(colorEnd[0]), title: Binding.constant(title[0]), colorTitle: Binding.constant(colorTitle[0]), threeLines: Binding.constant(threeLines[0]), green: Binding.constant(green[0]), yellow: Binding.constant(yellow[0]), icon: Binding.constant(icon[0]))
-                            .frame(width: (geometry.size.width / 2.2),
-                                   height: (geometry.size.height / 5))
-                        card(colorStart: Binding.constant(colorStart[1]), colorEnd: Binding.constant(colorEnd[1]), title: Binding.constant(title[1]), colorTitle: Binding.constant(colorTitle[1]), threeLines: Binding.constant(threeLines[1]), green: Binding.constant(green[1]), yellow: Binding.constant(yellow[1]), icon: Binding.constant(icon[1]))
-                            .frame(width: (geometry.size.width / 2.2),
-                                   height: (geometry.size.height / 5))
-                    }
-                    HStack{
-                        card(colorStart: Binding.constant(colorStart[2]), colorEnd: Binding.constant(colorEnd[2]), title: Binding.constant(title[2]), colorTitle: Binding.constant(colorTitle[2]), threeLines: Binding.constant(threeLines[2]), green: Binding.constant(green[2]), yellow: Binding.constant(yellow[2]), icon: Binding.constant(icon[2]))
-                            .frame(width: (geometry.size.width / 2.2),
-                                   height: (geometry.size.height / 5))
-                        
-                        card(colorStart: Binding.constant(colorStart[3]), colorEnd: Binding.constant(colorEnd[3]), title: Binding.constant(title[3]), colorTitle: Binding.constant(colorTitle[3]), threeLines: Binding.constant(threeLines[3]), green: Binding.constant(green[3]), yellow: Binding.constant(yellow[3]), icon: Binding.constant(icon[3]))
-                            .frame(width: (geometry.size.width / 2.2),
-                                   height: (geometry.size.height / 5))
-                    }
-                    .padding(.top, 40)
+        NavigationView{
+            VStack{
+                HStack{
+                    Text("O que vamos \naprender hoje?")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("TitleText"))
+                        .padding([.top, .leading], 22.0)
+                    Spacer()
                 }
-            }
+                GeometryReader { geometry in
+                    VStack{
+                        HStack{
+                            NavigationLink(
+                                destination: Text("aaaaaaaa"),
+                                label: {
+                                    card(colorStart: Binding.constant(colorStart[0]), colorEnd: Binding.constant(colorEnd[0]), title: Binding.constant(title[0]), colorTitle: Binding.constant(colorTitle[0]), threeLines: Binding.constant(threeLines[0]), green: Binding.constant(green[0]), yellow: Binding.constant(yellow[0]), icon: Binding.constant(icon[0]))
+                                        .frame(width: (geometry.size.width / 2.2),
+                                               height: (geometry.size.height / 5))
+                                })
+                            NavigationLink(
+                                destination: Text("bbbbbbbb"),
+                                label: {
+                                    card(colorStart: Binding.constant(colorStart[1]), colorEnd: Binding.constant(colorEnd[1]), title: Binding.constant(title[1]), colorTitle: Binding.constant(colorTitle[1]), threeLines: Binding.constant(threeLines[1]), green: Binding.constant(green[1]), yellow: Binding.constant(yellow[1]), icon: Binding.constant(icon[1]))
+                                        .frame(width: (geometry.size.width / 2.2),
+                                               height: (geometry.size.height / 5))
+                                })
+                        }
+                        HStack{
+                            NavigationLink(
+                                destination: Text("ccccccccc"),
+                                label: {
+                                    card(colorStart: Binding.constant(colorStart[2]), colorEnd: Binding.constant(colorEnd[2]), title: Binding.constant(title[2]), colorTitle: Binding.constant(colorTitle[2]), threeLines: Binding.constant(threeLines[2]), green: Binding.constant(green[2]), yellow: Binding.constant(yellow[2]), icon: Binding.constant(icon[2]))
+                                        .frame(width: (geometry.size.width / 2.2),
+                                               height: (geometry.size.height / 5))
+                                })
+                            NavigationLink(
+                                destination: Text("ddddddddd"),
+                                label: {
+                                    card(colorStart: Binding.constant(colorStart[3]), colorEnd: Binding.constant(colorEnd[3]), title: Binding.constant(title[3]), colorTitle: Binding.constant(colorTitle[3]), threeLines: Binding.constant(threeLines[3]), green: Binding.constant(green[3]), yellow: Binding.constant(yellow[3]), icon: Binding.constant(icon[3]))
+                                        .frame(width: (geometry.size.width / 2.2),
+                                               height: (geometry.size.height / 5))
+                                })
+                        }
+                        .padding(.top, 40)
+                    }
+                }
+            }.navigationBarHidden(true)
         }
     }
 }
--- Maozinha/Views/onboardingView.swift
+++ Maozinha/Views/onboardingView.swift
@@ -13,146 +13,151 @@ struct OnboardingView: View {
     let allSteps = 2
     
     var body: some View {
-        VStack() {
-            ProgressBarView(progress: $progress).frame(height: 16)
-            
-            ZStack {
-                if (currentStep == 0) {
-                    VStack {
-                        Image("onboarding1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding()
-                            .overlay(
-                                Image("PhoneFrame")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            )
-                            .frame(width: 400, height: 600
-                            )
-                            .overlay(
-                                GeometryReader { geometry in
-                                    
-                                    Circle()
-                                        .fill(Color("ButtonBackground"))
-                                        .opacity(0.4)
-                                        .frame(width: 45, height: 45)
-                                        .gesture(DragGesture()
-                                                    .onEnded({ (value) in
-                                                        if value.startLocation.y < value.location.y { moveToNextStep() }
-                                                    }))
-                                        .position(x: geometry.size.width * 0.75, y: geometry.size.height * 0.070)
-                                }
-                            ).padding(.top, 8)
-                        
-                        Spacer()
-                        
-                        Text("No Mãozinha, você interage com uma simulação nesta representação do seu iPhone.")
-                            .font(.system(size: 18))
-                            .fontWeight(.regular)
-                            .multilineTextAlignment(.center)
-                            .padding(8)
-                        Spacer()
-                        Text("Vamos aprender a ativar e desativar o modo \"Não perturbe\". Comece deslizando o dedo de cima para baixo no local indicado.")
-                            .font(.system(size: 18))
-                            .fontWeight(.bold)
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal, 12.0)
-                    }
-                    
-                }
-                if (currentStep == 1) {
-                    VStack {
-                        Image("onboarding2")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding()
-                            .overlay(
-                                Image("PhoneFrame")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            )
-                            .frame(width: 400, height: 600
-                            ).overlay(
-                                GeometryReader { geometry in
-                                    Button(action: moveToNextStep){
+        NavigationView{
+            VStack() {
+                ProgressBarView(progress: $progress).frame(height: 16)
+                
+                ZStack {
+                    if (currentStep == 0) {
+                        VStack {
+                            Image("onboarding1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding()
+                                .overlay(
+                                    Image("PhoneFrame")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                )
+                                .frame(width: 400, height: 600
+                                )
+                                .overlay(
+                                    GeometryReader { geometry in
+                                        
                                         Circle()
                                             .fill(Color("ButtonBackground"))
                                             .opacity(0.4)
                                             .frame(width: 45, height: 45)
+                                            .gesture(DragGesture()
+                                                        .onEnded({ (value) in
+                                                            if value.startLocation.y < value.location.y { moveToNextStep() }
+                                                        }))
+                                            .position(x: geometry.size.width * 0.75, y: geometry.size.height * 0.070)
                                     }
-                                    
-                                    .position(x: geometry.size.width * 0.43, y: geometry.size.height * 0.4225)
-                                }
-                            ).padding(.top, 8)
-                        
-                        Spacer()
-                        
-                        Text("À medida que você avança no tutorial, a barra de progresso no topo é preenchida.")
-                            .font(.system(size: 18))
-                            .fontWeight(.regular)
-                            .multilineTextAlignment(.center)
-                            .padding(8)
-                        
-                        Text("Toque no símbolo da lua para ativar o modo \"Não perturbe\" e finalizar o tutorial.")
-                            .font(.system(size: 18))
-                            .fontWeight(.bold)
-                            .multilineTextAlignment(.center)
-                            .padding(8)
-                    }
-                }
-                
-                
-                if (currentStep == 2) {
-                    VStack {
-                        Image("ThumbsUp")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding(64)
-                        Spacer()
-                        Text("Tudo certo!")
-                            .font(.system(size: 48))
-                            .fontWeight(.heavy)
-                            .foregroundColor(Color("TitleText"))
-                            .multilineTextAlignment(.center)
-                            .padding(16)
-                        Text("Você ativou o modo \"Não perturbe\" e concluiu o tutorial com sucesso!")
-                            .font(.system(size: 18))
-                            .fontWeight(.regular)
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal, 16.0)
-                        Spacer()
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            ZStack{
-                                Rectangle()
-                                    .frame(width: 300, height: 53, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                    .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonBackground")/*@END_MENU_TOKEN@*/)
-                                    .cornerRadius(10)
-                                Text("Concluir")
-                                    .font(.system(size: 17))
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonText")/*@END_MENU_TOKEN@*/)
-                            }
+                                ).padding(.top, 8)
                             
-                        })
-                        Spacer()
-                        Button(action: restartTutorial, label: {
-                            Text("Fazer novamente")
-                                .underline()
-                                .font(.system(size: 17))
+                            Spacer()
+                            
+                            Text("No Mãozinha, você interage com uma simulação nesta representação do seu iPhone.")
+                                .font(.system(size: 18))
+                                .fontWeight(.regular)
+                                .multilineTextAlignment(.center)
+                                .padding(8)
+                            Spacer()
+                            Text("Vamos aprender a ativar e desativar o modo \"Não perturbe\". Comece deslizando o dedo de cima para baixo no local indicado.")
+                                .font(.system(size: 18))
                                 .fontWeight(.bold)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/Color("LinkText")/*@END_MENU_TOKEN@*/)
-                        })
-                        Spacer()
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 12.0)
+                        }
+                        
+                    }
+                    if (currentStep == 1) {
+                        VStack {
+                            Image("onboarding2")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding()
+                                .overlay(
+                                    Image("PhoneFrame")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                )
+                                .frame(width: 400, height: 600
+                                ).overlay(
+                                    GeometryReader { geometry in
+                                        Button(action: moveToNextStep){
+                                            Circle()
+                                                .fill(Color("ButtonBackground"))
+                                                .opacity(0.4)
+                                                .frame(width: 45, height: 45)
+                                        }
+                                        
+                                        .position(x: geometry.size.width * 0.43, y: geometry.size.height * 0.4225)
+                                    }
+                                ).padding(.top, 8)
+                            
+                            Spacer()
+                            
+                            Text("À medida que você avança no tutorial, a barra de progresso no topo é preenchida.")
+                                .font(.system(size: 18))
+                                .fontWeight(.regular)
+                                .multilineTextAlignment(.center)
+                                .padding(8)
+                            
+                            Text("Toque no símbolo da lua para ativar o modo \"Não perturbe\" e finalizar o tutorial.")
+                                .font(.system(size: 18))
+                                .fontWeight(.bold)
+                                .multilineTextAlignment(.center)
+                                .padding(8)
+                        }
                     }
                     
                     
+                    if (currentStep == 2) {
+                        VStack {
+                            Image("ThumbsUp")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(64)
+                            Spacer()
+                            Text("Tudo certo!")
+                                .font(.system(size: 48))
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color("TitleText"))
+                                .multilineTextAlignment(.center)
+                                .padding(16)
+                            Text("Você ativou o modo \"Não perturbe\" e concluiu o tutorial com sucesso!")
+                                .font(.system(size: 18))
+                                .fontWeight(.regular)
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 16.0)
+                            Spacer()
+                            
+                            NavigationLink(
+                                destination: mainMenu()
+                                    .navigationBarHidden(true),
+                                label: {
+                                    ZStack{
+                                        Rectangle()
+                                            .frame(width: 300, height: 53, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonBackground")/*@END_MENU_TOKEN@*/)
+                                            .cornerRadius(10)
+                                        Text("Concluir")
+                                            .font(.system(size: 17))
+                                            .fontWeight(.semibold)
+                                            .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonText")/*@END_MENU_TOKEN@*/)
+                                    }
+                                })
+                            Spacer()
+                            Button(action: restartTutorial, label: {
+                                Text("Fazer novamente")
+                                    .underline()
+                                    .font(.system(size: 17))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/Color("LinkText")/*@END_MENU_TOKEN@*/)
+                            })
+                            Spacer()
+                        }
+                        
+                        
+                    }
+                    
                 }
-                
-            }
-            .animation(.easeInOut)
-        }.padding()
-        
+                .animation(.easeInOut)
+            }.padding()
+            .navigationBarHidden(true)
+        }
     }
     
     func moveToNextStep() {
