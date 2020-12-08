05577cce21a2b0293d7aa427f518878d73485553
Íris Soares <iss3@cin.ufpe.br>
2020-12-07 21:13:42 -0300

    integra com tela incial

---
+++ Maozinha/Views/introScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  introScreenView.swift
+//  Maozinha
+//
+//  Created by iris on 07/12/20.
+//
+
+import SwiftUI
+
+struct introScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct introScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        introScreenView()
+    }
+}
