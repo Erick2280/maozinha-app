67200e139174d9f00434bf47b0c8164575649e22
Erick Almeida <erickingles@gmail.com>
2020-12-09 17:22:09 -0300

    Finaliza o tutorial de alterar fonte

--- Maozinha/Views/TutorialViews/ChangeFontSettingsTutorialView.swift
+++ Maozinha/Views/TutorialViews/ChangeFontSettingsTutorialView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ChangeFontSettingsTutorialView: View {
     @State var progress = 0.0
     @State var currentStep = 0
-    let allSteps = 4
+    let allSteps = 5
     
     var body: some View {
         VStack() {
@@ -19,33 +19,36 @@ struct ChangeFontSettingsTutorialView: View {
             ZStack {
                 if (currentStep == 0) {
                     VStack {
-                        Image("SampleScreenshot")
+                        Image("onboarding1")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .padding()
                             .overlay(
-                            Image("PhoneFrame")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                        ).overlay(
-                            GeometryReader { geometry in
-                                Button(action: moveToNextStep) {
-                                    Circle()
-                                        .fill(Color("ButtonBackground"))
-                                        .opacity(0.4)
-                                        .frame(width: 50, height: 50)
+                                Image("PhoneFrame")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            )
+                            .frame(width: 400, height: 600
+                            ).overlay(
+                                GeometryReader { geometry in
+                                    Button(action: moveToNextStep){
+                                        Circle()
+                                            .fill(Color("ButtonBackground"))
+                                            .opacity(0.4)
+                                            .frame(width: 45, height: 45)
+                                    }
+                                    
+                                    .position(x: geometry.size.width * 0.575, y: geometry.size.height * 0.588)
                                 }
-                                    .position(x: geometry.size.width * 0.2, y: geometry.size.height * 0.9)
-                            }
-                        ).padding(.top, 8)
+                            ).padding(.top, 8)
                         
                         Spacer()
                         
                         Text("Neste tutorial, vamos aprender como mudar o tamanho do texto (ou fonte) e configurar para que fique mais confortável para você.")
                             .multilineTextAlignment(.center)
-                            .padding(8)
+                            .padding(.top, 8)
                         
-                        Text("Para começar, abra as Configurações (aplicativo com ícone de engrenagem).")
+                        Text("Para começar, abra os Ajustes (aplicativo com ícone de engrenagem).")
                             .bold()
                             .multilineTextAlignment(.center)
                             .padding(8)
@@ -54,12 +57,29 @@ struct ChangeFontSettingsTutorialView: View {
                 }
                 if (currentStep == 1) {
                     VStack {
-                        Button(action: moveToNextStep) {
-                            Circle()
-                                .fill(Color("ButtonBackground"))
-                                .opacity(0.4)
-                                .frame(width: 50, height: 50)
-                        }
+                        Image("ChangeFontSettings1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                            .overlay(
+                                Image("PhoneFrame")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            )
+                            .frame(width: 400, height: 600
+                            ).overlay(
+                                GeometryReader { geometry in
+                                    Button(action: moveToNextStep){
+                                        Circle()
+                                            .fill(Color("ButtonBackground"))
+                                            .opacity(0.4)
+                                            .frame(width: 24, height: 24)
+                                    }
+                                    
+                                    .position(x: geometry.size.width * 0.3, y: geometry.size.height * 0.76)
+                                }
+                            ).padding(.top, 8)
+                        
                         Spacer()
                         
                         Text("O aplicativo de Configurações abrirá, mostrando uma lista de opções.")
@@ -74,33 +94,109 @@ struct ChangeFontSettingsTutorialView: View {
                 }
                 if (currentStep == 2) {
                     VStack {
-                    Button(action: moveToNextStep) {
-                        Circle()
-                            .fill(Color("ButtonBackground"))
-                            .opacity(0.4)
-                            .frame(width: 50, height: 50)
-                    }
+                        Image("ChangeFontSettings2")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                            .overlay(
+                                Image("PhoneFrame")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            )
+                            .frame(width: 400, height: 600
+                            ).overlay(
+                                GeometryReader { geometry in
+                                    Circle()
+                                        .fill(Color("ButtonBackground"))
+                                        .opacity(0.4)
+                                        .frame(width: 45, height: 45)
+                                        .gesture(DragGesture()
+                                                    .onEnded({ (value) in
+                                                        if value.startLocation.y > value.location.y { moveToNextStep() }
+                                                    }))
+                                    
+                                    .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.75)
+                                }
+                            ).padding(.top, 8)
+                        
                     Spacer()
                     Text("Aparecerá uma tela com várias opções relacionadas à tela do seu celular, como brilho e fonte.")
                         .multilineTextAlignment(.center)
                         .padding(8)
                     
-                    Text("Selecione a opção \"Tamanho do Texto\".")
+                    Text("Role para baixo para ver mais opções.")
                         .bold()
                         .multilineTextAlignment(.center)
                         .padding(8)
                     }
 
                 }
-                
+
                 if (currentStep == 3) {
                     VStack {
-                    Button(action: moveToNextStep) {
-                        Circle()
-                            .fill(Color("ButtonBackground"))
-                            .opacity(0.4)
-                            .frame(width: 50, height: 50)
+                        Image("ChangeFontSettings3")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                            .overlay(
+                                Image("PhoneFrame")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            )
+                            .frame(width: 400, height: 600
+                            ).overlay(
+                                GeometryReader { geometry in
+                                    Button(action: moveToNextStep){
+                                        Circle()
+                                            .fill(Color("ButtonBackground"))
+                                            .opacity(0.4)
+                                            .frame(width: 24, height: 24)
+                                    }
+                                    
+                                    .position(x: geometry.size.width * 0.3, y: geometry.size.height * 0.875)
+                                }
+                            ).padding(.top, 8)
+                        
+                    Spacer()
+                    Text("Logo abaixo, existem mais opções para selecionar.")
+                        .multilineTextAlignment(.center)
+                        .padding(8)
+                    
+                    Text("Selecione a opção \"Tamanho do Texto\".")
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 32)
                     }
+                    Spacer()
+                }
+                
+                if (currentStep == 4) {
+                    VStack {
+                        Image("ChangeFontSettings4")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                            .overlay(
+                                Image("PhoneFrame")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            )
+                            .frame(width: 400, height: 600
+                            ).overlay(
+                                GeometryReader { geometry in
+                                    Circle()
+                                        .fill(Color("ButtonBackground"))
+                                        .opacity(0.4)
+                                        .frame(width: 45, height: 45)
+                                        .gesture(DragGesture()
+                                                    .onEnded({ (value) in
+                                                        if value.startLocation.x < value.location.x { moveToNextStep() }
+                                                    }))
+                                    
+                                    .position(x: geometry.size.width * 0.32, y: geometry.size.height * 0.877)
+                                }
+                            ).padding(.top, 8)
+                        
                     Spacer()
                     Text("Um controle deslizante aparecerá na tela, que pode ser usado para alterar o tamanho das letras.")
                         .multilineTextAlignment(.center)
@@ -114,7 +210,7 @@ struct ChangeFontSettingsTutorialView: View {
                     Spacer()
                 }
                 
-                if (currentStep == 4) {
+                if (currentStep == 5) {
                     VStack {
                     Image("ThumbsUp")
                         .resizable()
@@ -130,8 +226,33 @@ struct ChangeFontSettingsTutorialView: View {
                     Text("Você alterou o tamanho da letra do seu celular e concluiu o tutorial com sucesso!")
                         .multilineTextAlignment(.center)
                         .padding(8)
-                    }
                     Spacer()
+                    NavigationLink(
+                        destination: mainMenu()
+                            .navigationBarHidden(true),
+                        label: {
+                            ZStack{
+                                Rectangle()
+                                    .frame(width: 300, height: 53, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonBackground")/*@END_MENU_TOKEN@*/)
+                                    .cornerRadius(10)
+                                Text("Concluir")
+                                    .font(.system(size: 17))
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonText")/*@END_MENU_TOKEN@*/)
+                            }
+                        })
+                    Button(action: restartTutorial, label: {
+                        Text("Fazer novamente")
+                            .underline()
+                            .font(.system(size: 17))
+                            .fontWeight(.semibold)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/Color("LinkText")/*@END_MENU_TOKEN@*/)
+                    })
+                    .padding(.top, 10.0)
+                    Spacer()
+                    
+                    }
 
                 }
             
@@ -143,6 +264,11 @@ struct ChangeFontSettingsTutorialView: View {
     
     func moveToNextStep() {
         currentStep += 1
+        progress = (Double(currentStep) / Double(allSteps))
+    }
+    
+    func restartTutorial() {
+        currentStep = 0
         progress = (Double(currentStep) / Double(allSteps))
     }
 }
